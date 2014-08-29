edd6e68d995086644a541d78d56b21a338b23783
Ivan Murillo <murilloivan@gmail.com>
2014-08-29 20:51:22 +0200

    App's First version

---
+++ Films App/entertainment_center.py
@@ -0,0 +1,15 @@
+import media
+
+toy_story = media.Movie("Toy story",
+												"jajk kdja dh a", 
+												"aasdasdasd",
+												"https://www.youtube.com/watch?v=KYz2wyBy3kc")
+
+matrix = media.Movie("Matrix",
+												"jajk kdja dh a", 
+												"aasdasdasd",
+												"https://www.youtube.com/watch?v=m8e-FF8MsqU")
+
+matrix.play_trailer()
+
+
